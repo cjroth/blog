a945a469398bf0620ab7ea78dd1915d8d8e6fd7c
Chris Roth <chris@cjroth.com>
2025-12-10 23:10:34 -0500

    fix build

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -1,7 +1,7 @@
 import { HomeLayout } from 'fumadocs-ui/layouts/home';
 import { baseOptions } from '@/lib/layout.shared';
 
-export default function Layout({ children }: LayoutProps<'/docs'>) {
+export default function Layout({ children }: LayoutProps<'/blog'>) {
   return (
     <HomeLayout {...baseOptions()}>
       {children}
