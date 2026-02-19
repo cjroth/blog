2ee9b9adc168cbac03cf5e83831bd5a310abb39f
Chris Roth <chris@cjroth.com>
2026-02-19 00:02:13 -0500

    blog post

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { RootProvider } from 'fumadocs-ui/provider/next';
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import './global.css';
 
 export const metadata: Metadata = {
@@ -14,6 +15,11 @@ export default function Layout({ children }: LayoutProps<'/'>) {
     <html lang="en" suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
+        <Script
+          src="https://cloud.umami.is/script.js"
+          data-website-id="902cc0b0-0759-4e41-b653-99cf34b2fc59"
+          strategy="afterInteractive"
+        />
       </body>
     </html>
   );
